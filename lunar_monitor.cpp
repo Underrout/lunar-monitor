76ded6883d097de6219ab12e77c6a91f44642fd1
underway <underwayu@gmail.com>
2021-08-10 19:54:14 +0200

    Allow specification of ROM name, directory to watch, lunar magic path and level directory path via a lunar_monitor_config.txt file that should be placed in the same directory as the lunar magic executable used to edit levels (which should also contain usertoolbar.txt), monitor is now supplied with the lunar magic directory on startup so it can read this configuration file

--- lunar_monitor.cpp
+++ lunar_monitor.cpp
@@ -1,11 +1,7 @@
 // lunar-monitor-prototype-window.cpp : Defines the entry point for the application.
 //
 
-#define DIRECTORY_TO_WATCH ".\\"
-#define ROM_NAME_TO_WATCH "c.smc"
-#define LEVEL_DIRECTORY ".\\levels\\"
-#define LUNAR_MAGIC_PATH ".\\lunar_magic.exe"
-#define MWL_FILE_PREFIX "level "
+#define CONFIG_FILE_NAME "lunar_monitor_config.txt"
 
 #define DEBUG
 
@@ -29,6 +25,11 @@
 #include <cstdio>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <filesystem>
+#include <regex>
+
+namespace fs = std::filesystem;
 
 #ifdef DEBUG
 #include <cassert>
@@ -53,6 +54,7 @@
 #define FULL_ROM_PATH DIRECTORY_TO_WATCH ROM_NAME_TO_WATCH
 
 unsigned int lvlNum{ 0x105 };
+std::map<const std::string, const std::string> config;
 
 LPCWSTR szWndClass = SZ_WND_CLASS;
 
@@ -64,6 +66,10 @@ void AdjustConsoleBuffer(int16_t minLength);
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+std::wstring strToWstr(const std::string& s);
+std::map<const std::string, const std::string> getConfig(const fs::path& configFilePath);
+std::map<const std::string, const std::string> parseConfigFile(const fs::path& configFilePath);
+std::vector<std::wstring> getArgv();
 bool exportLvl(unsigned int lvlNum);
 std::time_t getLastModifiedTime(const std::string romPath);
 bool romWasModified(const std::string romPath, std::time_t& previousLastChangedTime);
@@ -77,9 +83,23 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+    std::vector<std::wstring> argv = getArgv();
+
 #ifdef DEBUG
     CreateNewConsole(500);
+
+    std::cout << "argv: ";
+
+    for (auto& arg : argv)
+        std::wcout << arg << " ";
+
+    std::cout << std::endl;
 #endif
+
+    fs::path configFilePath = argv.at(1);
+    configFilePath = configFilePath.parent_path() /= CONFIG_FILE_NAME;
+
+    config = getConfig(configFilePath);
 
     MSG msg;
 
@@ -104,16 +124,23 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
     if (!window)
     {
-        return 1;
+#ifdef DEBUG
+        std::cerr << "Failed to create window needed to receive Lunar Magic notifications, exiting" << std::endl;
+#endif
+        exit(1);
     }
 
     ShowWindow(window, SW_HIDE);
 
+    std::string dirToWatch = config.at("directory_to_watch");
+    std::wstring stemp = std::wstring(dirToWatch.begin(), dirToWatch.end());
+    LPCWSTR d = stemp.c_str();
+
     HANDLE romChangeHandle = FindFirstChangeNotification(
-        TEXT(DIRECTORY_TO_WATCH), false, FILE_NOTIFY_CHANGE_LAST_WRITE
+        d, false, FILE_NOTIFY_CHANGE_LAST_WRITE
     );
 
-    std::time_t previousLastModifiedTime = getLastModifiedTime(FULL_ROM_PATH);
+    std::time_t previousLastModifiedTime = getLastModifiedTime(config.at("rom_path"));
 
     while (true)
     {
@@ -124,7 +151,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 #ifdef DEBUG
             std::cout << "Change in ROM dir detected" << std::endl;
 #endif
-            handlePotentialROMChange(FULL_ROM_PATH, lvlNum, previousLastModifiedTime);
+            handlePotentialROMChange(config.at("rom_path"), lvlNum, previousLastModifiedTime);
         }
 
         FindNextChangeNotification(romChangeHandle);
@@ -154,13 +181,125 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             unsigned int newLvlNum = ((lParam >> 6) & 0x3FF);
 #ifdef DEBUG
-            std::cout << "LM now in level " << std::hex << std::uppercase << newLvlNum << std::endl;
+            std::cout << "LM now in level " << std::hex << std::uppercase << newLvlNum << std::dec << std::endl;
 #endif
             lvlNum = newLvlNum;
         }
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
+}
+
+std::wstring strToWstr(const std::string& s)
+{
+    std::wstring stemp = std::wstring(s.begin(), s.end());
+    return stemp.c_str();
+}
+
+std::string makePath(const fs::path& basePath, const std::string& s)
+{
+    fs::path p = basePath;
+    p /= s;
+    return p.string();
+}
+
+std::string fixPath(const fs::path& p)
+{
+    std::string f = p.string();
+    std::replace(f.begin(), f.end(), '/', '\\');
+    return f;
+}
+
+std::map<const std::string, const std::string> getConfig(const fs::path& configFilePath)
+{
+    std::map<const std::string, const std::string> parsedConfigFile = parseConfigFile(configFilePath);
+    std::map<const std::string, const std::string> config;
+
+    fs::path romPath = parsedConfigFile.at("rom_path");
+    const fs::path basePath = fixPath(romPath.parent_path());
+
+    for (const auto& [val, var] : parsedConfigFile)
+    {
+        if (val == "level_directory" || val == "lunar_magic_path")
+            config.insert(std::pair<const std::string, const std::string>(val, fixPath(makePath(basePath, var))));
+        else if (val == "rom_path")
+            config.insert(std::pair<const std::string, const std::string>(val, fixPath(var)));
+        else if (val == "mwl_file_format")
+            config.insert(std::pair<const std::string, const std::string>(val, var));
+        else
+            assert(false);  // Will only happen if a new config variable is added
+    }
+
+    config.insert(std::pair<const std::string, const std::string>("directory_to_watch", basePath.string()));
+
+#ifdef DEBUG
+    std::cout << "Loaded config: " << std::endl;
+
+    for (const auto& [val, var] : config)
+        std::cout << val << " -> " << var << std::endl;
+#endif
+
+    return config;
+}
+
+std::map<const std::string, const std::string> parseConfigFile(const fs::path& configFilePath)
+{
+    if (!fs::exists(configFilePath))
+    {
+#ifdef DEBUG
+        std::cerr << "ERROR: config file at " << configFilePath << " does not exist, exiting" << std::endl;
+#endif
+        exit(1);
+    }
+
+    std::map<const std::string, const std::string> config;
+    std::regex configValDefinition("(rom_path|level_directory|lunar_magic_path|mwl_file_format):\\s+\"(.+)\"");
+
+    std::ifstream file(configFilePath);
+    std::string currLine;
+    while (std::getline(file, currLine)) 
+    {
+        if (currLine.substr(0, 2) == "--")
+            continue;
+
+        std::smatch matchResults;
+        if (std::regex_match(currLine, matchResults, configValDefinition))
+        {
+#ifdef DEBUG
+            std::cout << "Config var found: " << matchResults[1] << " -> " << matchResults[2] << std::endl;
+#endif
+            std::string varName = matchResults[1];
+            std::string varVal = matchResults[2];
+
+            if (!config.count(varName))
+            {
+                config.insert(std::pair<const std::string, const std::string>(varName, varVal));
+            }
+            else
+            {
+#ifdef DEBUG
+                std::cerr << "Config var " << varName << "redefined, exiting" << std::endl;
+#endif
+                exit(1);
+            }
+        }
+    }
+
+    return config;
+}
+
+std::vector<std::wstring> getArgv()
+{
+    int argc;
+    LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+    std::vector<std::wstring> argvVec;
+    for (int i = 0; i != argc; ++i)
+        argvVec.push_back(argv[i]);
+
+    LocalFree(argv);
+
+    return argvVec;
 }
 
 bool exportLvl(unsigned int lvlNum)
@@ -170,16 +309,32 @@ bool exportLvl(unsigned int lvlNum)
 #endif
 
 #ifdef DEBUG
-    std::cout << "Attempting to export level " << std::hex << std::uppercase << lvlNum << std::endl;
+    std::cout << "Attempting to export level " << std::hex << std::uppercase << lvlNum << std::nouppercase << std::dec << std::endl;
 #endif
+
+    std::string mwlFileName = config.at("mwl_file_format");
+    int indexToInsertLvlNum = mwlFileName.find("#");
+
+    if (indexToInsertLvlNum != std::string::npos)
+    {
+        std::stringstream sstream;
+        sstream << std::hex << std::uppercase << lvlNum << std::nouppercase << std::dec;
+        std::string lvlNumString = sstream.str();
+
+        mwlFileName = std::regex_replace(mwlFileName, std::regex("\\#"), lvlNumString);
+    }
+
+    fs::path levelPath = config.at("directory_to_watch");
+    levelPath /= config.at("level_directory");
+    levelPath /= mwlFileName;
 
     std::wstringstream ws;
 
-    ws << " -ExportLevel \"" << FULL_ROM_PATH << "\" \"" 
-        << LEVEL_DIRECTORY MWL_FILE_PREFIX << std::hex << std::uppercase << lvlNum << std::nouppercase << ".mwl\" " << lvlNum;
+    ws << " -ExportLevel \"" << strToWstr(config.at("rom_path")) << "\" \"" << levelPath.wstring() << "\" " << std::hex
+        << std::uppercase << lvlNum << std::nouppercase << std::dec;
 
 #ifdef DEBUG
-    std::wcout << "Command:\n" << LUNAR_MAGIC_PATH << " " << ws.str() << std::endl;
+    std::wcout << "Command:\n" << strToWstr(config.at("lunar_magic_path")) << " " << ws.str() << std::endl;
 #endif
 
     std::wstring command = ws.str();
@@ -193,7 +348,7 @@ bool exportLvl(unsigned int lvlNum)
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
 
-    if (!CreateProcess(TEXT(LUNAR_MAGIC_PATH), buf.data(), NULL, NULL, false, 0, NULL, NULL, &si, &pi))
+    if (!CreateProcess(strToWstr(config.at("lunar_magic_path")).c_str(), buf.data(), NULL, NULL, false, 0, NULL, NULL, &si, &pi))
     {
 #ifdef DEBUG
         std::cerr << "Creating level export process failed: " << GetLastError() << std::endl;
@@ -213,8 +368,8 @@ bool exportLvl(unsigned int lvlNum)
 #ifdef DEBUG
     if (exitCode == 0)
     {
-        std::cout << "Successfully exported level " << std::hex << lvlNum << " as "
-            << LEVEL_DIRECTORY MWL_FILE_PREFIX << std::hex << std::uppercase << lvlNum << std::nouppercase << ".mwl" << std::endl;
+        std::cout << "Successfully exported level " << std::hex << std::uppercase << lvlNum << std::nouppercase << std::dec << " as "
+            << levelPath << std::endl;
         return true;
     }
     else
