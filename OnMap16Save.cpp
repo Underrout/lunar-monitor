4f17d8f65bfa1b92fdda05ffffa38f89f71f8ef4
underway <underwayu@gmail.com>
2022-02-17 22:02:37 +0100

    Replace command line invocation for map16 with direct call
    Makes map16 export much faster and should (hopefully) not have any adverse consequences.

--- OnMap16Save.cpp
+++ OnMap16Save.cpp
@@ -18,7 +18,7 @@ void OnMap16Save::onSuccessfulMap16Save(LM& lm, const Config& config)
     fs::path romPath = lm.getPaths().getRomDir();
     romPath += lm.getPaths().getRomName();
 
-    if (lm.getLevelEditor().exportMap16(lm.getPaths().getLmExePath(), romPath, config.getMap16Path()))
+    if (lm.getLevelEditor().exportMap16(config.getMap16Path()))
         Logger::log_message(L"Successfully exported map16 to \"%s\"", config.getMap16Path().c_str());
     else
         Logger::log_error(L"Failed to export map16");
